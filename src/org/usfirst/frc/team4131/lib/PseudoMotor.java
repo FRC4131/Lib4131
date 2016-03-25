229dd685a72d12da7b8efa6e46d23c2914d3d50a
Ian W <ian.uu12@gmail.com>
2016-03-24 20:02:44 -0700

    Update to 2016 libraries

--- src/org/usfirst/frc/team4131/lib/PseudoMotor.java
+++ src/org/usfirst/frc/team4131/lib/PseudoMotor.java
@@ -3,10 +3,16 @@ package org.usfirst.frc.team4131.lib;
 import edu.wpi.first.wpilibj.SpeedController;
 
 public class PseudoMotor implements SpeedController{
+	private boolean inverted = false;
 	private double value = 0;
+	public PseudoMotor(){this(false);}
+	public PseudoMotor(boolean inverted){setInverted(inverted);}
 	@Override public void pidWrite(double output){value = output;}
 	@Override public double get(){return value;}
 	@Override public void set(double speed, byte syncGroup){value = speed;}
 	@Override public void set(double speed){value = speed;}
 	@Override public void disable(){value = 0;}
+	@Override public void setInverted(boolean isInverted){inverted = isInverted;}
+	@Override public boolean getInverted(){return inverted;}
+	@Override public void stopMotor(){value = 0;}
 }
--- src/org/usfirst/frc/team4131/lib/WrappedController.java
+++ src/org/usfirst/frc/team4131/lib/WrappedController.java
@@ -5,13 +5,19 @@ import edu.wpi.first.wpilibj.SpeedController;
 public class WrappedController implements SpeedController{
 	private SpeedController motor;
 	private double mult;
-	public WrappedController(SpeedController motor, double mult){
+	public WrappedController(SpeedController motor, double mult){this(motor, mult, false);}
+	public WrappedController(SpeedController motor, boolean inverted){this(motor, 1, inverted);}
+	public WrappedController(SpeedController motor, double mult, boolean inverted){
 		this.motor = motor;
 		this.mult = mult;
+		setInverted(inverted);
 	}
 	@Override public void pidWrite(double output){motor.pidWrite(output * mult);}
 	@Override public double get(){return motor.get();}
 	@Override public void set(double speed, byte syncGroup){motor.set(speed * mult, syncGroup);}
 	@Override public void set(double speed){motor.set(speed * mult);}
 	@Override public void disable(){motor.disable();}
+	@Override public void setInverted(boolean isInverted){motor.setInverted(isInverted);}
+	@Override public boolean getInverted(){return motor.getInverted();}
+	@Override public void stopMotor(){motor.stopMotor();}
 }
